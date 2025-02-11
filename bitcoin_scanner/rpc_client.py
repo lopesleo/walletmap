b8b8b94331adb0ea358ed509d09a0886cc01685d
lopesleo <leonardo.lopes-almeida@hotmail.com>
2025-02-11 20:48:42 -0300

    fix: alterando url para indicar porta

--- bitcoin_scanner/rpc_client.py
+++ bitcoin_scanner/rpc_client.py
@@ -14,10 +14,10 @@ class RPCClient:
         self.headers = {'content-type': 'application/json'}
 
     def _get_default_url(self, network: str) -> str:
-        base_url = RPC_BASE_URL if RPC_BASE_URL.endswith('/') else f"{RPC_BASE_URL}/"
+        base_url = RPC_BASE_URL
         return {
-            'mainnet': f'{base_url}{MAINNET_RPC_PORT}/',
-            'testnet4': f'{base_url}{TESTNET4_RPC_PORT}/'
+            'mainnet': f'{base_url}:{MAINNET_RPC_PORT}/',
+            'testnet4': f'{base_url}:{TESTNET4_RPC_PORT}/'
         }[network]
 
     def call(self, method: str, params: list = None, retries: int = 3) -> Optional[Dict]:
